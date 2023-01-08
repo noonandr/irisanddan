7b58c52c645439a563c3002f854e045bfb7e2475
Daniel Noonan <noonan_dr@yahoo.co.uk>
2023-01-08 20:51:38 +0000

    feat(setup): creating Vue project

---
+++ src/components/HomeView.vue
@@ -0,0 +1,14 @@
+<template>
+  <div class="home-view">
+    <h1>Iris and Daniel</h1>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'HomeView'
+}
+</script>
+
+<style scoped lang="scss">
+</style>
